56864aff1ea5e68b65372fbddbcc0b8d23bc2061
McCloude <steve@jlsc.net>
2021-08-04 15:53:07 -0400

    fix factory

--- MsdProjectAuthService/src/main/java/com/bah/msd/domain/CustomerFactory.java
+++ MsdProjectAuthService/src/main/java/com/bah/msd/domain/CustomerFactory.java
@@ -7,7 +7,7 @@ public class CustomerFactory {
 	public static Customer getCustomer(String jsonString){
         JSONObject jsonObject = new org.json.JSONObject(jsonString); 
           
-        long id = (long) jsonObject.get("id");
+        int id = (int) jsonObject.get("id");
         String name = (String) jsonObject.get("name"); 
         String email = (String) jsonObject.get("email"); 
         String password = (String) jsonObject.get("password"); 
